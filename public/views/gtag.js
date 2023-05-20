e375547337936d1713104f278c486d8f506f34c7
cyberkutti-iedc <119793289+cyberkutti-iedc@users.noreply.github.com>
2023-05-20 12:52:39 +0530

    gtag.js

---
+++ public/views/gtag.js
@@ -0,0 +1,16 @@
+<!-- Google tag (gtag.js) -->
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-239736979-1"></script>
+
+<script>
+
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag(){dataLayer.push(arguments);}
+
+  gtag('js', new Date());
+
+  gtag('config', 'UA-239736979-1');
+
+</script>
+
